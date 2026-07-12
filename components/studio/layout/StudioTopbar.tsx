c9aac371468f6e5721da985cbfce51bfb09caee5
gustavo silva <guga.b.139935@gmail.com>
2026-07-12 00:37:34 -0400

    fix: add use client to studio topbar to fix vercel build

--- components/studio/layout/StudioTopbar.tsx
+++ components/studio/layout/StudioTopbar.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useEffect, useState } from "react"
 import { Undo, Redo, Download, Play, ChevronLeft, Image as ImageIcon, Loader2 } from "lucide-react"
 import { Button } from "@/components/ui/button"
